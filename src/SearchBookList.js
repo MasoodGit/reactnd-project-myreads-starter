a34b20472962c522fc04b6a6f64d2d202b51d44b
Masood_Alam <mohamed.masood.alam@philips.com>
2017-11-24 21:31:26 +0530

    Address review comments 1. Update search results books shelf value to 'None' if not on book shelves 2. Clear Search Results when query is cleared 3. Initiate data fetch ,as query is entered

--- src/SearchBookList.js
+++ src/SearchBookList.js
@@ -4,78 +4,85 @@ import Book from './Book'
 import * as BooksAPI from './BooksAPI'
 
 class SearchBookList extends React.Component {
-
   state = {
-    query: "",
+    query: '',
     searchResults: []
-  }
+  };
 
   componentWillUnmount() {
-    const searchResults = []
-    this.setState({searchResults})
+    this.clearSearchResults();
   }
 
-  updateQuery = (query) => {
-    this.setState({ query: query.trim()})
+  clearSearchResults() {
+    const searchResults = [];
+    this.setState({ searchResults });
   }
 
-  searchForBooks = (evt) => {
-    if ( evt.key === 'Enter') {
-      if(this.state.query) {
-        this.queryForBooks(this.state.query)
-      }
+  updateQuery = query => {
+    if (query === '' ) {
+      this.setState({query: '', searchResults: []});
+    } else {
+      this.setState({ query: query.trim() });
+      this.queryForBooks(query);
     }
-  }
+  };
 
   queryForBooks = (query) => {
     //make api call here
-    if (query) {
-      BooksAPI.search(query, 10).then(searchResults => {
-        if(searchResults.error) {
-          searchResults = []
-        } else {
-          searchResults.map(book => (this.props.myBooks
-                                      .filter( b => b.id === book.id)
-                                      .map( b => book.shelf = b.shelf)
-                                    )
-                            )
-        }
-
-        this.setState({searchResults})
-
-      })
-    }
-  }
+    BooksAPI.search(query, 10).then(searchResults => {
+      if (searchResults.error) {
+        searchResults = [];
+      } else {
+        searchResults.map(book => {
+          book.shelf = 'none';  // be default set shelf to none
+          return this.props.myBooks
+            .filter(b => b.id === book.id)
+            .map(b => (book.shelf = b.shelf));
+        });
+      }
+      // check if query is empty by the time data is fetched,
+      // thus avoiding condition where searchResults being
+      // set again after query is empty and searchResults set to []
+      // test with query "lin" and then "li" and "" in quick succession
+      if(!this.state.query) {
+        this.setState({ searchResults: [] });
+      } else {
+        this.setState({ searchResults });
+      }
+      console.log(`${this.state.query} and ${this.state.searchResults.length}`);
+    });
+};
 
   render() {
-    const books = this.state.searchResults
+    const books = this.state.searchResults;
     return (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to="/">Close</Link>
-              <div className="search-books-input-wrapper">
-                <input type="text"
-                  placeholder="Search by title or author"
-                  onKeyPress={this.searchForBooks}
-                  value= {this.state.query}
-                  onChange={(evt) => this.updateQuery(evt.target.value)}/>
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid">
-                {
-                  books.map(book => (
-                    <Book
-                      onUpdateBook={this.props.onUpdateBook}
-                      key={book.id}
-                      book={book}/>
-                  ))
-                }
-
-              </ol>
-            </div>
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              value={this.state.query}
+              onChange={evt => this.updateQuery(evt.target.value)}
+            />
+          </div>
         </div>
-    )
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {books.map(book => (
+              <Book
+                onUpdateBook={this.props.onUpdateBook}
+                key={book.id}
+                book={book}
+              />
+            ))}
+          </ol>
+        </div>
+      </div>
+    );
   } // end of render()
 }
 
